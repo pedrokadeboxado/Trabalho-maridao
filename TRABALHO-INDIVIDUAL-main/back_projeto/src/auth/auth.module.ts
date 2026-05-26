bd10579e325e89bd91ba83e00ac23342dc139eef
pedrokadeboxado <ens-pedrocavalheiro@ugv.edu.br>
2026-05-25 22:05:26 -0300

    adicionando swagger

--- TRABALHO-INDIVIDUAL-main/back_projeto/src/auth/auth.module.ts
+++ TRABALHO-INDIVIDUAL-main/back_projeto/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 @Module({
   imports: [UsuariosModule],
@@ -9,4 +10,15 @@ import { AuthService } from './auth.service';
   providers: [AuthService],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+  static setupSwagger(app) {
+    const options = new DocumentBuilder()
+      .setTitle('API de Autenticação')
+      .setDescription('Documentação da API de autenticação')
+      .setVersion('1.0')
+      .addTag('auth')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+}
--- TRABALHO-INDIVIDUAL-main/back_projeto/src/main.ts
+++ TRABALHO-INDIVIDUAL-main/back_projeto/src/main.ts
@@ -1,14 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Esta linha permite que o React se conecte
+
+  app.enableCors();
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Usuários')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
+
   console.log(`Aplicação rodando na porta ${port}`);
+  console.log(`Swagger disponível em: http://localhost:${port}/swagger`);
 }
+
 bootstrap();
